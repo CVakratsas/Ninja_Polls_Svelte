8125bc781c63b7bf538ddff598d5ba3b71834195
Constantinos Vakratsas <c.vakratsas@gmail.com>
2024-02-01 19:25:39 +0200

    Poll list and Poll details created

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import Footer from "./components/Footer.svelte";
   import Tabs from "./shared/Tabs.svelte";
   import CreatePollForm from "./components/CreatePollForm.svelte";
+  import PollList from "./components/PollList.svelte";
 
   // Tabs
   let items = ['Current Polls', 'Add New Poll'];
@@ -10,15 +11,50 @@
   const tabChange = (e) => {
     activeItem = e.detail;
   }
+
+  // polls
+  let polls = [
+    {
+      id: 1,
+      question: 'Python or JavaScript?',
+      answerA: 'Python',
+      answerB: 'JavaScript',
+      votesA: 9,
+      votesB: 15
+    }
+  ];
+
+  const handleAdd = (e) => {
+    const poll = e.detail;
+    polls = [poll, ...polls];
+    console.log(polls);
+    activeItem = 'Current Polls';
+  }
+
+  const handleVote = (e) => {
+    const {id, option} = e.detail;
+
+    let copiedPolls = [...polls];
+    let upvotedPoll = copiedPolls.find((poll) => poll.id == id);
+
+    if (option === 'a') {
+      upvotedPoll.votesA++;
+    }
+    if (option === 'b') {
+      upvotedPoll.votesB++;
+    }
+
+    polls = copiedPolls;
+  }
 </script>
 
 <Header />
 <main>
   <Tabs {activeItem} {items} on:tabChange={tabChange}/>
   {#if activeItem === 'Current Polls'}
-    <p>Poll list components goes here</p>
+    <PollList {polls} on:vote={handleVote}/>
   {:else if activeItem === 'Add New Poll'}
-    <CreatePollForm />
+    <CreatePollForm on:add={handleAdd}/>
   {/if}
 </main>
 <Footer />
--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,25 +1,62 @@
 <script>
+    import { createEventDispatcher } from "svelte";
     import Button from "../shared/Button.svelte";
+
+    let dispatch = createEventDispatcher();
     
     let fields = { question: '', answerA: '', answerB: '' };
+    let errors = { question: '', answerA: '', answerB: '' };
+    let valid = false;
   
     let submitHandler = () => {
-      console.log(fields);
+        valid = true;
+      // validate question
+      if (fields.question.trim().length < 5) {
+        valid = false;
+        errors.question = 'Question must be at least 5 characters long';
+      } else {
+        errors.question = '';
+      }
+
+      // validate answer A
+      if (fields.answerB.trim().length < 1) {
+        valid = false;
+        errors.answerA = 'Answer A can not be empty';
+      } else {
+        errors.answerA = '';
+      }
+
+      // validate answer B
+      if (fields.answerB.trim().length < 1) {
+        valid = false;
+        errors.answerB = 'Answer B can not be empty';
+      } else {
+        errors.answerB = '';
+      }
+
+      // add new poll
+      if (valid) {
+        let poll = {...fields, votesA: 0, votesB: 0, id: Math.random()};
+        dispatch('add', poll);
+      }
     }
   </script>
   
   <form on:submit|preventDefault={submitHandler}>
     <div class="form-field">
-      <label for="question">Poll Question:</label>
-      <input type="text" id="question" bind:value={fields.question}>
+        <label for="question">Poll Question:</label>
+        <input type="text" id="question" bind:value={fields.question}>
+        <div class="error">{errors.question}</div>
     </div>
     <div class="form-field">
-      <label for="answer-a">Answer A value:</label>
-      <input type="text" id="answer-a" bind:value={fields.answerA}>
+        <label for="answer-a">Answer A value:</label>
+        <input type="text" id="answer-a" bind:value={fields.answerA}>
+        <div class="error">{errors.answerA}</div>
     </div>
     <div class="form-field">
-      <label for="answer-b">Answer B value:</label>
-      <input type="text" id="answer-b" bind:value={fields.answerB}>
+        <label for="answer-b">Answer B value:</label>
+        <input type="text" id="answer-b" bind:value={fields.answerB}>
+        <div class="error">{errors.answerB}</div>
     </div>
     <Button type="secondary" flat={true}>Add Poll</Button>
   </form>
@@ -44,5 +81,11 @@
     label {
         margin: 10px auto;
         text-align: left;
+    }
+
+    .error {
+        font-weight: bold;
+        font-size: 12px;
+        color: #d91b42;
     }
   </style>
