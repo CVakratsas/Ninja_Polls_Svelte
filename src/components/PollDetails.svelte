2b11038e16fed86f12f307c0f80c222cb55a9b99
Constantinos Vakratsas <c.vakratsas@gmail.com>
2024-02-02 18:43:40 +0200

    Delete poll button

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,6 +1,7 @@
 <script>
   import PollStore from "../stores/PollStore";
   import Card from "../shared/Card.svelte";
+  import Button from "../shared/Button.svelte";
 
   export let poll;
 
@@ -24,6 +25,13 @@
       return copiedPolls;
     });
   }
+
+  // deleting a poll
+  const handleDelete = (id) => {
+    PollStore.update(currentPolls => {
+      return currentPolls.filter(poll => poll.id != id)
+    });
+  };
 </script>
 
 <Card>
@@ -41,6 +49,9 @@
         <div class="answer" on:click={() => handleVote('b', poll.id)}>
             <div class="percent percent-b" style="width: {percentB}%"></div>
             <span>{poll.answerB} ({poll.votesB})</span>
+        </div>
+        <div class="delete">
+          <Button flat={true} on:click={() => handleDelete(poll.id)}>Delete</Button>
         </div>
     </div>
 </Card>
@@ -88,5 +99,10 @@
     .percent-b {
       border-left: 4px solid #34c496;
       background: rgba(69, 196, 150, 0.2);
+    }
+
+    .delete {
+      margin-top: 30px;
+      text-align: center;
     }
   </style>
